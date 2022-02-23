add7552e622427edd99ea25d4e14847bd68c62d3
sumitgarudkar <90499296+sumitgarudkar@users.noreply.github.com>
2022-02-23 10:34:36 +0530

    Time: 44 ms (30.22%), Space: 19.3 MB (58.17%) - LeetHub

--- 283-move-zeroes/283-move-zeroes.cpp
+++ 283-move-zeroes/283-move-zeroes.cpp
@@ -3,13 +3,12 @@ public:
     void moveZeroes(vector<int>& nums) {
         int it=0;
         for(int i=0; i<nums.size(); i++){
-            if(nums[i]!=0){
+            if(nums[i]!=0){ 
                 nums[it++]=nums[i];
             }
         }
-        while(it<nums.size()){
-            nums[it]=0;
-            it++;
+        for(int i=it; i<nums.size(); i++){
+            nums[i]=0;
         }
     }
 };
