30f898114b40d6fcc668386860ad596e07dd681e
sumitgarudkar <90499296+sumitgarudkar@users.noreply.github.com>
2022-02-24 14:04:54 +0530

    Time: 8 ms (98.83%), Space: 9.6 MB (90.15%) - LeetHub

---
+++ 557-reverse-words-in-a-string-iii/557-reverse-words-in-a-string-iii.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    string reverseWords(string s) {
+        for(int i=0; i<s.size(); i++){
+            if(s[i]!=' '){
+                int j=i;
+                while(j<s.size() && s[j]!=' ') j++;
+                reverse(s.begin()+i,s.begin()+j);
+                i=j;
+            }
+        }
+        return s;
+    }
+};
