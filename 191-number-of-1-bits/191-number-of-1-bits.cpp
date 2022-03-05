7f2df484bb2e730af05773a20872365cc244d6b2
sumitgarudkar <90499296+sumitgarudkar@users.noreply.github.com>
2022-03-05 17:21:49 +0530

    Time: 0 ms (100.00%), Space: 6 MB (48.06%) - LeetHub

--- 191-number-of-1-bits/191-number-of-1-bits.cpp
+++ 191-number-of-1-bits/191-number-of-1-bits.cpp
@@ -3,9 +3,7 @@ public:
     int hammingWeight(uint32_t n) {
         int res=0;
         while(n){
-            if(n&1){
-                ++res;
-            }
+            if(n&1) ++res;
             n=n>>1;
         }
         return res;
