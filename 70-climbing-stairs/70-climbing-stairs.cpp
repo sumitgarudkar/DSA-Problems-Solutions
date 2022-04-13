2359e85a4abb3d3216d81de11ff05be615483240
sumitgarudkar <90499296+sumitgarudkar@users.noreply.github.com>
2022-04-13 08:24:54 +0530

    Time: 0 ms (100.00%), Space: 6.3 MB (15.49%) - LeetHub

--- 70-climbing-stairs/70-climbing-stairs.cpp
+++ 70-climbing-stairs/70-climbing-stairs.cpp
@@ -3,12 +3,12 @@ public:
     int climbStairs(int n) {
         if(n<=0) return 0;
         if(n==1) return 1;
-        if(n==2) return 2;
-        vector<int> res(45);
-        res[0]=1;res[1]=2;
-        for(int i=2; i<n; i++){
-            res[i]+=res[i-1]+res[i-2];
+        vector<int> dp(n+1,0);
+        dp[0]=1;
+        dp[1]=1;
+        for(int i=2; i<=n; i++){
+            dp[i]=dp[i-1]+dp[i-2];
         }
-        return res[n-1];
+        return dp[n];
     }
 };
