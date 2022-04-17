669282fb5082ab2b6c6202a748bba0f2632a89b7
sumitgarudkar <90499296+sumitgarudkar@users.noreply.github.com>
2022-04-17 23:49:31 +0530

    Time: 453 ms (75.26%), Space: 167.3 MB (49.01%) - LeetHub

--- 2150-find-all-lonely-numbers-in-the-array/2150-find-all-lonely-numbers-in-the-array.cpp
+++ 2150-find-all-lonely-numbers-in-the-array/2150-find-all-lonely-numbers-in-the-array.cpp
@@ -1,15 +1,17 @@
 class Solution {
 public:
     vector<int> findLonely(vector<int>& nums) {
-        int n=nums.size();
-        if(n==1) return {nums[0]};
-        sort(nums.begin(),nums.end());
         vector<int> res;
-        if(nums[0]!=nums[1] && nums[0]!=nums[1]-1) res.push_back(nums[0]);
-        if(nums[n-1]!=nums[n-2] && nums[n-2]!=nums[n-1]-1) res.push_back(nums[n-1]);
-        for(int i=1; i<n-1; i++){
-            if(nums[i]==nums[i-1] || nums[i]==nums[i+1] || nums[i]+1==nums[i+1] || nums[i-1]+1==nums[i]) continue;
-            else res.push_back(nums[i]);
+        unordered_map<int,int> mp;
+        for(auto i:nums) mp[i]++;
+        
+        for(auto m: mp){
+            if(m.second>1 || mp.find(m.first-1)!=mp.end() || mp.find(m.first+1)!=mp.end()){
+                continue;
+            }
+            else{
+                res.push_back(m.first);
+            }
         }
         return res;
     }
