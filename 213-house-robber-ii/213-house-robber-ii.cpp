76136af68051e7b5c697d175cd2f2c2d4aca9871
sumitgarudkar <90499296+sumitgarudkar@users.noreply.github.com>
2022-07-24 23:25:44 +0530

    Time: 8 ms (8.84%), Space: 7.9 MB (41.88%) - LeetHub

--- 213-house-robber-ii/213-house-robber-ii.cpp
+++ 213-house-robber-ii/213-house-robber-ii.cpp
@@ -1,32 +1,25 @@
 class Solution {
-public:
-    int robbingHouse(vector<int> &nums){
-        int n=nums.size();
-        int prev2=0, prev=nums[0];
-        for(int i=1; i<n; i++){
-            int take = nums[i];
-            if(i>1) take+=prev2;
-            int notTake = prev;
-            int curi=max(take,notTake);
-            prev2 =prev;
-            prev = curi;
-        }
-        return prev;
+    int help(int n, vector<int> &nums, vector<int> &dp){
+        if(n==0) return nums[0];
+        if(n<0) return 0;
+        if(dp[n]!=-1) return dp[n];
+        int pick=nums[n]+help(n-2,nums,dp);
+        int notPick=help(n-1,nums,dp);
+        return dp[n]=max(pick,notPick);
     }
-    
-    
+public:
     int rob(vector<int>& nums) {
         int n=nums.size();
-         if (n < 2){
-            if(n) return nums[0];
-            else return 0;
-         }
-        vector<int> tmp1;
-        vector<int> tmp2;
+        if(n<=1) return nums[0];
+        vector<int> dp(n+1,-1);
+        vector<int> first,second;
         for(int i=0; i<n; i++){
-            if(i!=0) tmp1.push_back(nums[i]);
-            if(i!=n-1) tmp2.push_back(nums[i]);
+            if(i!=0) first.push_back(nums[i]);
+            if(i!=n-1) second.push_back(nums[i]);
         }
-        return max(robbingHouse(tmp1),robbingHouse(tmp2));
+        int one = help(n-2,first,dp);
+        for(int i=0; i<n+1; i++) dp[i]=-1;
+        int two = help(n-2,second,dp);
+        return max(one,two);
     }
 };
