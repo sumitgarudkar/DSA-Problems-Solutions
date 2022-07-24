5929add61ee9befddbf617f02363c77486927ca9
sumitgarudkar <90499296+sumitgarudkar@users.noreply.github.com>
2022-07-25 00:29:47 +0530

    Time: 0 ms (100.00%), Space: 8 MB (18.56%) - LeetHub

--- 213-house-robber-ii/213-house-robber-ii.cpp
+++ 213-house-robber-ii/213-house-robber-ii.cpp
@@ -1,25 +1,28 @@
 class Solution {
-    int help(int n, vector<int> &nums, vector<int> &dp){
-        if(n==0) return nums[0];
-        if(n<0) return 0;
-        if(dp[n]!=-1) return dp[n];
-        int pick=nums[n]+help(n-2,nums,dp);
-        int notPick=help(n-1,nums,dp);
-        return dp[n]=max(pick,notPick);
+    int help(vector<int> nums){
+        int n=nums.size();
+        int prev=nums[0], prevPast=0;
+        for(int i=1; i<n; i++){
+            int pick = nums[i]+prevPast;
+            int notPick = prev;
+            prevPast = prev;
+            prev=max(pick,notPick);
+        }
+        return prev;
     }
+    
 public:
     int rob(vector<int>& nums) {
         int n=nums.size();
-        if(n<=1) return nums[0];
-        vector<int> dp(n+1,-1);
+        if (n < 2){
+            if(n) return nums[0];
+            else return 0;
+         }
         vector<int> first,second;
         for(int i=0; i<n; i++){
             if(i!=0) first.push_back(nums[i]);
             if(i!=n-1) second.push_back(nums[i]);
         }
-        int one = help(n-2,first,dp);
-        for(int i=0; i<n+1; i++) dp[i]=-1;
-        int two = help(n-2,second,dp);
-        return max(one,two);
+        return max(help(first),help(second));
     }
 };
